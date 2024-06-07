23561ea3b0005d89e2dd392ae36ee141a87f1ddb
J-Rupendra <rupendraj21@gmail.com>
2024-06-07 09:26:58 +0530

    JSX syntax

--- app.js
+++ app.js
@@ -12,5 +12,15 @@ const heading = React.createElement(
 // we need a root element to render this created element
 // React DOM will give us the ability to assign root and rendering
 
+//JSX will be converted into react element and can be rendered using react dom
+const jsxElement = <h1>this is from JSX</h1>
+const jsxElementMultiLine = (<h1 className="head">
+    this is from JSX</h1>)
+
+// functional component is a JS function which starts with capital letter and returns single either react element or JSX
+const ElementFunctionalComponent = () => {
+    return <h1>This is a functional component</h1>
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root")) // as React DOM give the ability to manipulate th DOM
 root.render(heading) // it actually converts the heading object into html tag and attach it to the root element.
